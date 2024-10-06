3b89829afeefd6bf2145de481e921734e76767da
Leo Boitel <x>
2024-10-07 01:18:15 +0200

    Implemented Jaccard distance and tests. Left todo - behaviour for sequences shorter than k (now still returning a 0-padded kmer, probably not what we want)

--- TP/__main__.py
+++ TP/__main__.py
@@ -1,36 +1,51 @@
 from TP.loading import load_directory
 from TP.kmers import stream_kmers, kmer2str, encode_kmer
-
+from sys import stderr
 
 
 def jaccard(fileA, fileB, k):
-    #open file : 
 
-    print(fileA)
-    
+    union_count = 0
+    intersection_count = 0
+    kmer_counts_A = {}
 
-    j = 0
-    
-    return j
+    for seqA in fileA:
+        for kmer_A in stream_kmers(seqA, k):
+            if kmer_A not in kmer_counts_A:
+                kmer_counts_A[kmer_A] = 0
+            kmer_counts_A[kmer_A] += 1
+            union_count += 1
 
+    for seqB in fileB:
+        for kmer_B in stream_kmers(seqB, k):
+            if kmer_B in kmer_counts_A and kmer_counts_A[kmer_B] > 0:
+                kmer_counts_A[kmer_B] -= 1
+                intersection_count += 1
+            else:
+                union_count += 1
+
+    if union_count == 0: # checking to avoid an error with empty/short sequences
+        print("Warning: no k-mers to compare.", file=stderr)
+        return 0
+
+    return intersection_count / union_count
 
 
 if __name__ == "__main__":
 
-    pass
+    print("Computation of Jaccard similarity between files")
 
-    # print("Computation of Jaccard similarity between files")
-
-    # # Load all the files in a dictionary
-    # files = load_directory("data")
-    # k = 21
+    # Load all the files in a dictionary
+    files = load_directory("data")
+    k = 21
     
-    # print("Computing Jaccard similarity for all pairs of samples")
-    # filenames = list(files.keys())
-    # for i in range(len(files)):
-    #     for j in range(i+1, len(files)):
+    print("Computing Jaccard similarity for all pairs of samples")
+    filenames = list(files.keys())
+    for i in range(len(files)):
+        for j in range(i+1, len(files)):
             
-    #         # --- Complete here ---
+            # --- Complete here ---
+            # TODO what is there to complete here ?
 
-    #         j = jaccard(files[filenames[i]], files[filenames[j]], k)
-    #         print(filenames[i], filenames[j], j)
+            distance = jaccard(files[filenames[i]], files[filenames[j]], k)
+            print(filenames[i], filenames[j], distance)
---
+++ tests_TP.py
@@ -0,0 +1,34 @@
+from TP.kmers import kmer2str, encode_kmer_forward_sense, encode_kmer_reverse_complement, encode_kmer
+from TP.kmers import stream_kmers
+from TP.__main__ import jaccard # in a big project we would move tested functions out of __main__
+# to avoid importing from it, but in our case this seemed simpler than changing the TP file structure.
+
+# Single kmer encoding
+assert kmer2str(encode_kmer_forward_sense("ATATCGCAA", 7),7)      == "ATATCGC"
+assert kmer2str(encode_kmer_reverse_complement("ATATCGCAA", 7),7) == "GCGATAT"
+assert kmer2str(encode_kmer("ATATCGCAA", 7),7)                    == "ATATCGC"
+assert kmer2str(encode_kmer("GCGATATAA", 7),7)                    == "ATATCGC"
+
+
+# Kmer stream encoding
+seq = "ATATCGCAAAAAGAGAAATTCAGAGAGACT"
+for k in range(1, len(seq) + 1):
+    encoded_stream = list(stream_kmers(seq, k))
+    encoded_one_by_one = [encode_kmer(seq[i:i+k], k) for i in range(len(seq) + 1 - k)]
+    assert encoded_stream == encoded_one_by_one # Python list equality => all elements equal
+
+
+# Jaccard distance testing
+## Sanity checks
+seq1 = "ATATCGCAAAAAGAGAAATTCAGAGAGACTATATCGCAAAAAGAGAAATTCAGAGAGACT"
+assert jaccard([seq1], [seq1], 5) == 1.0
+seq2 = "GGGAGGGTGGCCGAGAAAAAAAAGTGTGAGC"
+assert jaccard([seq1], [seq2], 5) == jaccard([seq2], [seq1], 5)
+assert jaccard([seq1, seq1], [seq2, seq2], 5) == jaccard([seq1], [seq2], 5)
+assert jaccard([seq1], [seq2], len(seq2)) == 0.0
+
+## Simple manual tests
+assert jaccard(["AAAT"], ["AAGGG"], 2) == 1 / 6
+assert jaccard(["TTTA"], ["AAGGG"], 2) == 1 / 6
+
+print("All tests passed.")
