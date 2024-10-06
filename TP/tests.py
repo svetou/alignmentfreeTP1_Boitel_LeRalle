d5c69a2f2892e5f706b6f35061bfcd317a365a96
Leo Boitel <x>
2024-10-07 00:00:32 +0200

    Changed encode_kmer_reverse to use first k nucs of seq in reverse order (instead of last). Updated tests to sequences longer than k to test for this behaviour

--- TP/tests.py
+++ TP/tests.py
@@ -1,8 +1,9 @@
-from kmers import kmer2str, encode_kmer_positive_sense, encode_kmer_reverse_complement, encode_kmer
+from kmers import kmer2str, encode_kmer_forward_sense, encode_kmer_reverse_complement, encode_kmer
 
-assert kmer2str(encode_kmer_positive_sense("ATATCGC", 7),7)     == "ATATCGC"
-assert kmer2str(encode_kmer_reverse_complement("ATATCGC", 7),7) == "GCGATAT"
-assert kmer2str(encode_kmer("ATATCGC", 7),7)                    == "ATATCGC"
-assert kmer2str(encode_kmer("GCGATAT", 7),7)                    == "ATATCGC"
+# Single kmer encoding
+assert kmer2str(encode_kmer_forward_sense("ATATCGCAA", 7),7)      == "ATATCGC"
+assert kmer2str(encode_kmer_reverse_complement("ATATCGCAA", 7),7) == "GCGATAT"
+assert kmer2str(encode_kmer("ATATCGCAA", 7),7)                    == "ATATCGC"
+assert kmer2str(encode_kmer("GCGATATAA", 7),7)                    == "ATATCGC"
 
 print("All tests passed.")
